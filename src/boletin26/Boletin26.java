2ea2ecba412c984197b4d847b2bab917e8f1584d
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2019-04-02 11:11:26 +0200

    Terminado

---
+++ src/boletin26/Boletin26.java
@@ -0,0 +1,17 @@
+
+
+package boletin26;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class Boletin26 {
+
+ 
+    public static void main(String[] args) {
+       Boletin26_Paleta ventana = new Boletin26_Paleta();
+       ventana.setVisible(true);
+    }
+
+}
